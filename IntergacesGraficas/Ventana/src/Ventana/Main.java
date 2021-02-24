8bab2e71f382aec579f5053e0d53c35c63e2b9ec
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-02-24 17:38:21 -0600

    Ventanas - Cerrar y agregar titulo

--- IntergacesGraficas/Ventana/src/Ventana/Main.java
+++ IntergacesGraficas/Ventana/src/Ventana/Main.java
@@ -8,7 +8,7 @@ package Ventana;
 public class Main {
     public static void main(String[] args) {
         Ventana v1 = new Ventana();
-        
         v1.setVisible(true); // Se hace visible la ventana
+        
     }
 }
--- IntergacesGraficas/Ventana/src/Ventana/Ventana.java
+++ IntergacesGraficas/Ventana/src/Ventana/Ventana.java
@@ -8,6 +8,7 @@ public class Ventana extends JFrame{
     
     public Ventana(){
         setSize(500, 500); // Se establece el tamano de la ventana
-        
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // Finaliza el programa al cerrar la ventana
+        setTitle("Ejemplo Ventana"); // Establece el titulo superior de la ventana
     }
 }
