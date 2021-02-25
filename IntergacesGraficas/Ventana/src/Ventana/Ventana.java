a20e62cf005a2bc7857c83f2fd03100aa7583e14
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-02-24 18:02:33 -0600

    Ventanas - Posicion inicial de la venta

--- IntergacesGraficas/Ventana/src/Ventana/Ventana.java
+++ IntergacesGraficas/Ventana/src/Ventana/Ventana.java
@@ -8,7 +8,11 @@ public class Ventana extends JFrame{
     
     public Ventana(){
         setSize(500, 500); // Se establece el tamano de la ventana
-        setDefaultCloseOperation(EXIT_ON_CLOSE); // Finaliza el programa al cerrar la ventana
         setTitle("Ejemplo Ventana"); // Establece el titulo superior de la ventana
+        //setLocation(100, 200);// Establece la locacion donde aparacera la ventana
+        //setBounds(100, 200, 500, 500); // Estable el tamano y establece la locacion a la vez
+        setLocationRelativeTo(null); // Con el argumento null establece la locacion en el centro de la pantalla
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // Finaliza el programa al cerrar la ventana
+
     }
 }
