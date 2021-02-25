9c1d6076bbeb3e147ffd76e0df4315344f486464
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-02-25 16:09:09 -0600

    Ventanas - Agregar panel, etiqueta. Modificar layout y posiciones de los componentes

--- IntergacesGraficas/Ventana/src/Ventana/Ventana.java
+++ IntergacesGraficas/Ventana/src/Ventana/Ventana.java
@@ -5,6 +5,7 @@ package Ventana;
 import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Ventana extends JFrame{
     
@@ -17,7 +18,21 @@ public class Ventana extends JFrame{
         
         //setResizable(false); // Evita que el usuario pueda cambiar el tamaño de la ventana
         setMinimumSize(new Dimension(300, 300));
-        this.getContentPane().setBackground(Color.BLUE); // Establecer el color de la ventana     
+        // this.getContentPane().setBackground(Color.BLUE); // Establecer el color de la ventana     
+        iniciarComponentes();
         setDefaultCloseOperation(EXIT_ON_CLOSE); // Finaliza el programa al cerrar la ventana
+    }
+    
+    // Creamos un metodo para crear un Panel - esto para tener ordenado
+    private void iniciarComponentes(){
+        JPanel panel = new JPanel();
+        //panel.setBackground(Color.DARK_GRAY); // Se establece el color del panel
+        panel.setLayout(null);
+        this.getContentPane().add(panel); // Se agrega el panel a la ventana
+        
+        JLabel etiqueta = new JLabel();
+        etiqueta.setText("Hola");
+        etiqueta.setBounds(50, 20, 100, 20);
+        panel.add(etiqueta);
     }
 }
