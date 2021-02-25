5cce7f970e21ce0be8eaf7467951a5031577b7f1
Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2021-02-24 19:07:08 -0600

    Ventanas - Redimensionar y color de ventana

--- IntergacesGraficas/Ventana/src/Ventana/Ventana.java
+++ IntergacesGraficas/Ventana/src/Ventana/Ventana.java
@@ -2,6 +2,8 @@
 // Heredar la clase JFrame
 package Ventana;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Ventana extends JFrame{
@@ -12,7 +14,10 @@ public class Ventana extends JFrame{
         //setLocation(100, 200);// Establece la locacion donde aparacera la ventana
         //setBounds(100, 200, 500, 500); // Estable el tamano y establece la locacion a la vez
         setLocationRelativeTo(null); // Con el argumento null establece la locacion en el centro de la pantalla
+        
+        //setResizable(false); // Evita que el usuario pueda cambiar el tamaño de la ventana
+        setMinimumSize(new Dimension(300, 300));
+        this.getContentPane().setBackground(Color.BLUE); // Establecer el color de la ventana     
         setDefaultCloseOperation(EXIT_ON_CLOSE); // Finaliza el programa al cerrar la ventana
-
     }
 }
